65b6c4119eb16cb50e89ae49f9582826f46e1e05
Nancy Yang <iamgoodworkers@gmail.com>
2011-09-28 17:18:45 +0800

    add util

--- conf/auto-complete-settings.el
+++ conf/auto-complete-settings.el
@@ -1,30 +1,14 @@
-(require 'auto-complete-yasnippet)
+;;(require 'auto-complete-yasnippet)
 
 ;;(add-to-list 'ac-dictionary-directories myrobotech-emacs-auto-complete-dict-path)
 (require 'auto-complete-config)
-(ac-config-default)
+;;(ac-config-default)
 (global-auto-complete-mode 1)
-;(defun auto-complete-settings ()
-  "Settings for `auto-complete'."
-  ;; After do this, isearch any string, M-: (match-data) always
-  ;; return the list whose elements is integer
-  (global-auto-complete-mode 1)
- (global-auto-complete-mode)
-  ;; 不让回车的时候执行`ac-complete', 因为当你输入完一个
-  ;; 单词的时候, 很有可能补全菜单还在, 这时候你要回车的话,
-  ;; 必须要干掉补全菜单, 很麻烦, 用M-j来执行`ac-complete'
-  (apply-define-key
-   ac-complete-mode-map
-   `(("<return>"   nil)
-     ("RET"        nil)
-     ("M-j"        ac-complete)
-     ("<C-return>" ac-complete)
-     ("M-n"        ac-next)
-     ("M-p"        ac-previous)))
- 
-  (setq ac-dwim t)
-  (setq ac-candidate-max ac-candidate-menu-height)
-;) 
+(eal-define-keys                                        
+ 'ac-complete-mode-map                                  
+ `(("<return>"   nil)                                   
+   ("RET"        nil)                                   
+   ("M-j"        ac-complete)))  
  
 
 (provide 'auto-complete-settings)
